fe55f92826a6c3d0809bc1c1ff98b0b4d344ebe7
Krzysztof Rakowski <krzysztof@rakowski.pl>
2013-04-13 18:27:17 +0200

    task 2.1, part 2

--- ruby/task_2_1.rb
+++ ruby/task_2_1.rb
@@ -1,6 +1,8 @@
 # Print the contents of an array of sixteen number, four numbers
 # at a time, using just "each".
 
+puts "Using each:"
+
 a = (1..16).to_a
 c = 0
 p = []
@@ -11,7 +13,7 @@ a.each do |i|
     if c == 4 then 
         c = 0
         p.each { |j| print j, ' '}
-        print "\n"
+        puts 
         p.clear
     end
 end
@@ -20,4 +22,9 @@ end
 
 # Now, do the same with each_slice in Enumerable.
 
+puts "Using Enumerable and each_slice:"
 
+(1..16).each_slice(4) do |s|
+    s.each { |j| print j, ' '}
+    puts
+end
