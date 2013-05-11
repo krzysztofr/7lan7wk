f616d34dd79cd0a78b644cf41e0215d082b669e3
Krzysztof Rakowski <krzysztof@rakowski.pl>
2013-05-11 02:03:16 +0200

    Prolog, Day 1, task 1.1 completed

---
+++ prolog/task_1_1.pl
@@ -0,0 +1,11 @@
+book(misery, king).
+book(carrie, king).
+book(shining, king).
+book(long_walk, king).
+book(long_walk, bachman).
+book(running_man, bachman).
+book(solaris, lem).
+book(invicible, lem).
+book(eden, lem).
+
+% run: book(Name, king).
