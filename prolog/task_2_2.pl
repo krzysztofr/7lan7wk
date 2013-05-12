7912eadbd2b6b95e99fcce0ccddcfd524ed600a9
Krzysztof Rakowski <krzysztof@rakowski.pl>
2013-05-12 19:14:24 +0200

    Prolog, Day2, task 2.2 - find the smallest element of a list + bonus (largest element).

---
+++ prolog/task_2_2.pl
@@ -0,0 +1,23 @@
+% Find the smallest/largest element of a list.
+
+max(A,B,R) :- A>B, R is A.
+max(A,B,R) :- A=<B, R is B.
+
+% one element list
+maxlist([Head|Tail], R) :- length(Tail, L), L=0, R=Head.
+
+maxlist([Head|Tail], R) :- maxlist(Tail, R1), max(R1, Head, R).
+
+% to test, run:
+% maxlist([1,2,3,4], R).
+
+min(A,B,R) :- A>B, R is B.
+min(A,B,R) :- A=<B, R is A.
+
+% one element list
+minlist([Head|Tail], R) :- length(Tail, L), L=0, R=Head.
+
+minlist([Head|Tail], R) :- minlist(Tail, R1), min(R1, Head, R).
+
+% to test, run:
+% minlist([1,2,3,4], R).
