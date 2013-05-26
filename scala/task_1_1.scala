be880db512ddf3beda0fb9aaac8d70f6e4f9bcdf
Krzysztof Rakowski <krzysztof@rakowski.pl>
2013-05-26 22:38:51 +0200

    Scala Day 1, task 1.1 template added

---
+++ scala/task_1_1.scala
@@ -0,0 +1,7 @@
+/*
+Write a game that will take a tic-tac-toe board with X, O and blank characters
+and detect the winner or whether there is a tie or no winner yey. Use classes
+where appropriate.
+*/
+
+
