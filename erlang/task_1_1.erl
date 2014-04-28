5a128a852ba8b7d261bb41e4e5df2533ba3421a5
Krzysztof Rakowski <krzysztof@rakowski.pl>
2014-04-28 15:30:08 +0200

    Erland day 1 task 1 - fixed couting words (was couting characters)

--- erlang/task_1_1.erl
+++ erlang/task_1_1.erl
@@ -1,7 +1,10 @@
 % Write a function that uses recursion to return the number of words in a string.
 
 -module(task_1_1).
+-export([count_words/1]).
 -export([count/1]).
 
-count([One]) -> 1; 
-count([Head|Tail]) -> 1 + count(Tail).
+count_words(Sentence) -> count(string:tokens(Sentence, " ")).
+
+count([]) -> 0; 
+count([_|Tail]) -> 1 + count(Tail).
