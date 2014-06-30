8cf1321111a387a7723347ec3e043f596d4f60c3
Krzysztof Rakowski <krzysztof@rakowski.pl>
2014-06-30 17:44:42 +0200

    Clojure day 1 task 1

--- clojure/day1/src/day1/core.clj
+++ clojure/day1/src/day1/core.clj
@@ -1,6 +1,14 @@
 (ns day1.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn big
+  "Returns true if string st is longer than n characters."
+  [st n]
+  (> (count st) n)
+ )
+
+(defn main
+	"Main function."
+	[]
+	(println (big "mystring" 3) (big "mystring" 10))
+)
+
